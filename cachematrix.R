277fa962beeb016d9946c2b9297dad653486c625
amandapeel <amandapeel@users.noreply.github.com>
2014-08-07 19:55:45 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## https://class.coursera.org/rprog-006/forum/thread?thread_id=93 for help
+## https://class.coursera.org/rprog-006/forum/thread?thread_id=96
 
 ## Write a short comment describing this function
 
