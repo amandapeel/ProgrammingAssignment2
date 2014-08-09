f7034ec4e0985f8bce3b1afe3b32a6ffeb41e335
amandapeel <amandapeel@users.noreply.github.com>
2014-08-08 20:55:15 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-## https://class.coursera.org/rprog-006/forum/thread?thread_id=93 for help
-## https://class.coursera.org/rprog-006/forum/thread?thread_id=96
+## These are a set of functions designed to first transform a matrix
+## such that it can cache its inverse, and second to cache the inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix object that can cache the matrix's (x) inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(mean) m <<- mean
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve computes and caches inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
 
-##potato
+##The following will run the program on a sample set
+##> a <- c(1,2,0)
+##> b <- c(0,1,9)
+##> c <- c(5,0,4)
+##> mtx <- matrix(c(a,b,c), nrow=3, ncol=3)
+##> mtx2 <- makeCacheMatrix(mtx)
+##> cachesolve(mtx2)
